c2441b5f9ae017841391e4bdcc859605036e0f35
Ajinkya5050 <92012301+Ajinkya5050@users.noreply.github.com>
2021-12-24 13:30:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Testtodo from "./components/testtodo";
+import Todo from "./components/todo";
+
+function App() {
+  return (
+    <div>
+      <Testtodo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
